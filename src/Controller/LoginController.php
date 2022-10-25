90493ad3ce6e2561c67ae61f890875e2cb5b2102
Shanbora <chanbora@logilink.fr>
2022-10-25 15:17:08 +0200

    add logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,17 +10,27 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
+    #[Route('/connexion', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
 
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login/index.html.twig', [
+        return $this->render('app/security/index.html.twig', [
             'controller_name' => 'LoginController',
             'last_username' => $lastUsername,
             'error' => $error
         ]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        throw new Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
