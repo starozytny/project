974c864482932719f8dd85c986246008256b796f
Shanbora <chanbora@logilink.fr>
2022-12-16 17:41:35 +0100

    add search function

---
+++ assets/common/js/functions/search.js
@@ -0,0 +1,37 @@
+const Sanitaze = require('@commonFunctions/sanitaze')
+
+function search(type, dataImmuable, search) {
+    let newData = [];
+    search = search.toLowerCase();
+    search = Sanitaze.removeAccents(search);
+    newData = dataImmuable.filter(function(v) {
+        return switchFunction(type, search, v);
+    })
+
+    return newData;
+}
+
+function searchStartWith (value, search){
+    let val = Sanitaze.removeAccents(value);
+    return val.startsWith(search)
+}
+
+function switchFunction(type, search, v) {
+    switch (type) {
+        case "user":
+            if(searchStartWith(v.username.toLowerCase(), search)
+                || searchStartWith(v.email.toLowerCase(), search)
+                || searchStartWith(v.firstname.toLowerCase(), search)
+                || searchStartWith(v.lastname.toLowerCase(), search)
+            ){
+                return v;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+module.exports = {
+    search
+}
