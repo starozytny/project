6918ceea7f3d254db6de6a777aa99bbdf324f259
Shanbora <chanbora@logilink.fr>
2022-12-20 16:16:52 +0100

    menu work

--- assets/admin/js/app.js
+++ assets/admin/js/app.js
@@ -1,1 +1,17 @@
 import '../css/app.scss';
+
+menu();
+
+function menu() {
+    let btn = document.querySelector('.nav-mobile');
+    if(btn){
+        btn.addEventListener('click', function () {
+            let content = document.querySelector('.nav-content');
+            if(content.classList.contains('active')){
+                content.classList.remove('active');
+            }else{
+                content.classList.add('active');
+            }
+        })
+    }
+}
