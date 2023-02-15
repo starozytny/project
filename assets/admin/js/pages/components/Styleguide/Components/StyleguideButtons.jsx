f337d18f99e12c65f24a365e6719aac60a77b1ca
Shanbora <chanbora@logilink.fr>
2023-02-15 15:39:50 +0100

    buttons components

--- assets/admin/js/pages/components/Styleguide/Components/StyleguideButtons.jsx
+++ assets/admin/js/pages/components/Styleguide/Components/StyleguideButtons.jsx
@@ -1,31 +1,124 @@
 import React from "react";
 
-import { Alert } from "@commonComponents/Elements/Alert";
+import {Button, ButtonIcon, TxtButton} from "@commonComponents/Elements/Button";
 
 export function StyleguideButtons ()
 {
-    let data = "Lorem ipsum";
 
    return <div className="styleguide-content-col">
        <section className="styleguide-section">
            <div className="styleguide-section-title">Basic</div>
            <div className="styleguide-section-content">
-               <Alert type="info">{data}</Alert>
-               <Alert type="warning">{data}</Alert>
-               <Alert type="danger">{data}</Alert>
-               <Alert type="success">{data}</Alert>
-               <Alert type="grey4">{data}</Alert>
+               <div className="styleguide-buttons-line">
+                   <Button>Default</Button>
+                   <Button type="primary">Primary</Button>
+                   <Button type="warning">Warning</Button>
+                   <Button type="danger">Danger</Button>
+                   <Button type="success">Success</Button>
+                   <Button type="grey3">Grey3</Button>
+                   <Button type="grey4">Grey4</Button>
+               </div>
+               <div className="styleguide-buttons-line">
+                   <Button icon="star">Default</Button>
+                   <Button icon="star" />
+               </div>
+               <div className="styleguide-buttons-line">
+                   <Button icon="chart-3" isLoader={true}>Default</Button>
+                   <Button icon="chart-3" isLoader={true} loaderWithText={true}>Default</Button>
+                   <Button icon="chart-3" isLoader={true} />
+               </div>
+           </div>
+       </section>
+       <section className="styleguide-section">
+           <div className="styleguide-section-title">Basic outline</div>
+           <div className="styleguide-section-content">
+               <div className="styleguide-buttons-line">
+                   <Button outline={true}>Default</Button>
+                   <Button outline={true} type="primary">Primary</Button>
+                   <Button outline={true} type="warning">Warning</Button>
+                   <Button outline={true} type="danger">Danger</Button>
+                   <Button outline={true} type="success">Success</Button>
+                   <Button outline={true} type="grey3">Grey3</Button>
+                   <Button outline={true} type="grey4">Grey4</Button>
+               </div>
+               <div className="styleguide-buttons-line">
+                   <Button outline={true} icon="star">Default</Button>
+                   <Button outline={true} icon="star" />
+               </div>
+               <div className="styleguide-buttons-line">
+                   <Button outline={true} icon="chart-3" isLoader={true}>Default</Button>
+                   <Button outline={true} icon="chart-3" isLoader={true} loaderWithText={true}>Default</Button>
+                   <Button outline={true} icon="chart-3" isLoader={true} />
+               </div>
+           </div>
+       </section>
+       <section className="styleguide-section">
+           <div className="styleguide-section-title">Text</div>
+           <div className="styleguide-section-content">
+               <div className="styleguide-buttons-line">
+                   <TxtButton >Default</TxtButton>
+                   <TxtButton type="primary">Primary</TxtButton>
+                   <TxtButton type="warning">Warning</TxtButton>
+                   <TxtButton type="danger">Danger</TxtButton>
+                   <TxtButton type="success">Success</TxtButton>
+                   <TxtButton type="grey3">Grey3</TxtButton>
+                   <TxtButton type="grey4">Grey4</TxtButton>
+               </div>
+               <div className="styleguide-buttons-line">
+                   <TxtButton icon="star">Default</TxtButton>
+                   <TxtButton icon="star" />
+               </div>
+               <div className="styleguide-buttons-line">
+                   <TxtButton icon="chart-3" isLoader={true}>Default</TxtButton>
+                   <TxtButton icon="chart-3" isLoader={true} loaderWithText={true}>Default</TxtButton>
+                   <TxtButton icon="chart-3" isLoader={true} />
+               </div>
            </div>
        </section>
 
        <section className="styleguide-section">
-           <div className="styleguide-section-title">Options</div>
+           <div className="styleguide-section-title">Icons</div>
            <div className="styleguide-section-content">
-               <Alert type="info" icon="star">{data}</Alert>
-               <Alert type="info" withIcon={false}>{data}</Alert>
-               <Alert type="info" withIcon={false} canClose={true}>{data}</Alert>
-               <Alert type="info" title="Title">{data}</Alert>
-               <Alert type="info" title="Title" withIcon={false}>{data}</Alert>
+               <div className="styleguide-buttons-line">
+                   <ButtonIcon icon="home">Default</ButtonIcon>
+                   <ButtonIcon icon="question" type="primary">Primary</ButtonIcon>
+                   <ButtonIcon icon="warning" type="warning">Warning</ButtonIcon>
+                   <ButtonIcon icon="error" type="danger">Danger</ButtonIcon>
+                   <ButtonIcon icon="check1" type="success">Success</ButtonIcon>
+                   <ButtonIcon icon="star" type="grey3">Grey3</ButtonIcon>
+                   <ButtonIcon icon="like" type="grey4">Grey4</ButtonIcon>
+               </div>
+               <div className="styleguide-buttons-line">
+                   <ButtonIcon icon="star">Default</ButtonIcon>
+                   <ButtonIcon icon="star" />
+               </div>
+               <div className="styleguide-buttons-line">
+                   <ButtonIcon icon="chart-3" isLoader={true}>Default</ButtonIcon>
+                   <ButtonIcon icon="chart-3" isLoader={true} />
+               </div>
+           </div>
+       </section>
+
+       <section className="styleguide-section">
+           <div className="styleguide-section-title">Icons outline</div>
+           <div className="styleguide-section-content">
+               <div className="styleguide-buttons-line">
+                   <ButtonIcon outline={true} icon="home">Default</ButtonIcon>
+                   <ButtonIcon outline={true} icon="question" type="primary">Primary</ButtonIcon>
+                   <ButtonIcon outline={true} icon="warning" type="warning">Warning</ButtonIcon>
+                   <ButtonIcon outline={true} icon="error" type="danger">Danger</ButtonIcon>
+                   <ButtonIcon outline={true} icon="check1" type="success">Success</ButtonIcon>
+                   <ButtonIcon outline={true} icon="star" type="grey3">Grey3</ButtonIcon>
+                   <ButtonIcon outline={true} icon="like" type="grey4">Grey4</ButtonIcon>
+               </div>
+               <div className="styleguide-buttons-line">
+                   <ButtonIcon outline={true} icon="star">Default</ButtonIcon>
+                   <ButtonIcon outline={true} icon="star" />
+               </div>
+               <div className="styleguide-buttons-line">
+                   <ButtonIcon outline={true} icon="chart-3" isLoader={true}>Default</ButtonIcon>
+                   <ButtonIcon outline={true} icon="chart-3" isLoader={true} />
+               </div>
            </div>
        </section>
    </div>
--- assets/common/js/components/Elements/Button.jsx
+++ assets/common/js/components/Elements/Button.jsx
@@ -1,41 +1,66 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-export function ButtonIcon(props){
-    const { icon, children, text, onClick, outline=false,
-        type="default", isSubmit=false, element="button", target="_self", download=false, tooltipWidth=null } = props;
+export function Button(props){
+    const { icon, type="default", isSubmit=false, outline=false, children, onClick, element="button", target="_self",
+        isLoader=false, loaderWithText=false } = props;
 
-    let divStyle = tooltipWidth ? { width: tooltipWidth + "px" } : null;
+    let loaderClasse = isLoader ? " btn-loader-" + (loaderWithText ? "with-text" : "without-text") : ""
 
     if(element === "button"){
-        return <button className={`btn-icon btn-icon-${outline ? "outline-" : ""}${type}`} type={isSubmit ? "submit" : "button"} onClick={onClick}>
-            <span className={`icon-${icon}`} />
-            {text && <span>{text}</span>}
-            {children && <span className="tooltip" style={divStyle}>{children}</span>}
+        return <button className={`btn${loaderClasse} btn-${outline ? "outline-" : ""}${type}`}
+                       type={isSubmit ? "submit" : "button"} onClick={onClick}>
+            {icon && <span className={`icon-${icon}`} />}
+            {children && <span>{children}</span>}
         </button>
     }else{
-        return <a className={`btn-icon btn-icon-${outline ? "outline-" : ""}${type}`} target={target} download={download} href={onClick}>
-            <span className={`icon-${icon}`} />
-            {text && <span>{text}</span>}
-            {children && <span className="tooltip" style={divStyle}>{children}</span>}
+        return <a className={`btn${loaderClasse} btn-${outline ? "outline-" : ""}${type}`}
+                  target={target} href={onClick}>
+            {icon && <span className={`icon-${icon}`} />}
+            {children && <span>{children}</span>}
         </a>
     }
 }
 
-export function Button(props){
-    const { icon, type="default", isSubmit=false, outline=false, children, onClick, element="button", target="_self", isLoader=false } = props;
+export function TxtButton(props){
+    const { icon, type="default", isSubmit=false, children, onClick, element="button", target="_self",
+        isLoader=false, loaderWithText=false } = props;
+
+    let loaderClasse = isLoader ? " btn-loader-" + (loaderWithText ? "with-text" : "without-text") : ""
 
     if(element === "button"){
-        return <button className={`btn ${isLoader ? "btn-loader" : ""} btn-${outline ? "outline-" : ""}${type}`}
+        return <button className={`txt-button${loaderClasse} txt-button-${type}`}
                        type={isSubmit ? "submit" : "button"} onClick={onClick}>
             {icon && <span className={`icon-${icon}`} />}
-            <span>{children}</span>
+            {children && <span>{children}</span>}
         </button>
     }else{
-        return <a className={`btn ${isLoader ? "btn-loader" : ""} btn-${outline ? "outline-" : ""}${type}`}
+        return <a className={`txt-button${loaderClasse} txt-button-${type}`}
                   target={target} href={onClick}>
             {icon && <span className={`icon-${icon}`} />}
-            <span>{children}</span>
+            {children && <span>{children}</span>}
+        </a>
+    }
+}
+
+export function ButtonIcon(props){
+    const { icon, children, text, onClick, outline=false,
+        type="default", isSubmit=false, element="button", target="_self", download=false, tooltipWidth=null, isLoader=false } = props;
+
+    let divStyle = tooltipWidth ? { width: tooltipWidth + "px" } : null;
+    let loaderClasse = isLoader ? " btn-loader-without-text" : ""
+
+    if(element === "button"){
+        return <button className={`btn-icon${loaderClasse} btn-icon-${outline ? "outline-" : ""}${type}`} type={isSubmit ? "submit" : "button"} onClick={onClick}>
+            <span className={`icon-${icon}`} />
+            {text && <span>{text}</span>}
+            {children && <span className="tooltip" style={divStyle}>{children}</span>}
+        </button>
+    }else{
+        return <a className={`btn-icon${loaderClasse} btn-icon-${outline ? "outline-" : ""}${type}`} target={target} download={download} href={onClick}>
+            <span className={`icon-${icon}`} />
+            {text && <span>{text}</span>}
+            {children && <span className="tooltip" style={divStyle}>{children}</span>}
         </a>
     }
 }
@@ -72,7 +97,23 @@ Button.propTypes = {
     ]),
     element: PropTypes.string,
     target: PropTypes.string,
-    isLoader: PropTypes.bool
+    isLoader: PropTypes.bool,
+    loaderWithText: PropTypes.bool
+}
+
+TxtButton.propTypes = {
+    icon: PropTypes.string,
+    type: PropTypes.string,
+    isSubmit: PropTypes.bool,
+    children: PropTypes.node,
+    onClick: PropTypes.oneOfType([
+        PropTypes.node,
+        PropTypes.func,
+    ]),
+    element: PropTypes.string,
+    target: PropTypes.string,
+    isLoader: PropTypes.bool,
+    loaderWithText: PropTypes.bool
 }
 
 ButtonIcon.propTypes = {
@@ -89,6 +130,7 @@ ButtonIcon.propTypes = {
     element: PropTypes.string,
     target: PropTypes.string,
     tooltipWidth: PropTypes.number,
+    isLoader: PropTypes.bool,
 }
 
 
