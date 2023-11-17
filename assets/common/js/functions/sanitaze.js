366dfeab23fd4d414829995ece675d47a7578d8f
Shanbora <chanbora@logilink.fr>
2023-11-17 15:59:52 +0100

    move sanitaze function intern

--- assets/common/js/functions/sanitaze.js
+++ assets/common/js/functions/sanitaze.js
@@ -18,6 +18,38 @@ function sanitizeString(chaine){
     return chaine;
 }
 
+function addZeroToNumber (data) {
+    return data > 9 ? data : "0" + data;
+}
+
+function capitalize(elem) {
+    if(elem.length !== 0){
+        let first = elem.substring(0, 1);
+        elem = elem.substring(1);
+        elem = first.toUpperCase() + elem;
+    }
+
+    return elem;
+}
+
+function removeAccents (str) {
+    const accentsMap = {
+        a: 'á|à|ã|â|À|Á|Ã|Â',
+        e: 'é|è|ê|É|È|Ê',
+        i: 'í|ì|î|Í|Ì|Î',
+        o: 'ó|ò|ô|õ|Ó|Ò|Ô|Õ',
+        u: 'ú|ù|û|ü|Ú|Ù|Û|Ü',
+        c: 'ç|Ç',
+        n: 'ñ|Ñ',
+    };
+
+    for (let pattern in accentsMap) {
+        str = str.replace(new RegExp(accentsMap[pattern], 'g'), pattern);
+    }
+
+    return str;
+}
+
 function toFormatPhone(elem){
     if(elem !== "" && elem !== undefined && elem !== null){
         let arr = elem.match(/[0-9-+]/g);
@@ -64,38 +96,6 @@ function toFormatBytesToSize(bytes) {
     if (bytes === 0) return '0 Byte';
     let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
     return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-}
-
-function addZeroToNumber (data) {
-    return data > 9 ? data : "0" + data;
-}
-
-function capitalize(elem) {
-    if(elem.length !== 0){
-        let first = elem.substring(0, 1);
-        elem = elem.substring(1);
-        elem = first.toUpperCase() + elem;
-    }
-
-    return elem;
-}
-
-function removeAccents (str) {
-    const accentsMap = {
-        a: 'á|à|ã|â|À|Á|Ã|Â',
-        e: 'é|è|ê|É|È|Ê',
-        i: 'í|ì|î|Í|Ì|Î',
-        o: 'ó|ò|ô|õ|Ó|Ò|Ô|Õ',
-        u: 'ú|ù|û|ü|Ú|Ù|Û|Ü',
-        c: 'ç|Ç',
-        n: 'ñ|Ñ',
-    };
-
-    for (let pattern in accentsMap) {
-        str = str.replace(new RegExp(accentsMap[pattern], 'g'), pattern);
-    }
-
-    return str;
 }
 
 function toFormatCalendar(value, retour = "") {
