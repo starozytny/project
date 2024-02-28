ef9933a02fe5768d7860b32fd3989faf918e5005
shanbora <chanbora@logilink.fr>
2024-02-28 16:10:47 +0100

    rename layout components

--- assets/admin/js/pages/components/Changelogs/Changelogs.jsx
+++ assets/admin/js/pages/components/Changelogs/Changelogs.jsx
@@ -13,108 +13,118 @@ import { ModalDelete } from "@commonComponents/Shortcut/Modal";
 import { LoaderElements } from "@commonComponents/Elements/Loader";
 import { Pagination, TopSorterPagination } from "@commonComponents/Elements/Pagination";
 
-const URL_GET_DATA        = "intern_api_changelogs_list";
-const URL_DELETE_ELEMENT  = "intern_api_changelogs_delete";
+const URL_GET_DATA = "intern_api_changelogs_list";
+const URL_DELETE_ELEMENT = "intern_api_changelogs_delete";
 
 const SESSION_PERPAGE = "project.perpage.changelogs";
 const SESSION_FILTERS = "project.filters.changelogs";
 
 export class Changelogs extends Component {
-    constructor(props) {
-        super(props);
+	constructor (props) {
+		super(props);
 
-        let saveNbPerPage = sessionStorage.getItem(SESSION_PERPAGE);
-        let perPage = saveNbPerPage !== null ? parseInt(saveNbPerPage) : 20;
+		let saveNbPerPage = sessionStorage.getItem(SESSION_PERPAGE);
+		let perPage = saveNbPerPage !== null ? parseInt(saveNbPerPage) : 20;
 
-        let saveFilters = props.highlight ? sessionStorage.getItem(SESSION_FILTERS) : null;
+		let saveFilters = props.highlight ? sessionStorage.getItem(SESSION_FILTERS) : null;
 
-        this.state = {
-            perPage: perPage,
-            currentPage: 0,
-            sorter: Sort.compareCreatedAtInverse,
-            loadingData: true,
-            filters: saveFilters !== null ? JSON.parse(saveFilters) : [],
-            element: null,
-        }
+		this.state = {
+			perPage: perPage,
+			currentPage: 0,
+			sorter: Sort.compareCreatedAtInverse,
+			loadingData: true,
+			filters: saveFilters !== null ? JSON.parse(saveFilters) : [],
+			element: null,
+		}
 
-        this.pagination = React.createRef();
-        this.delete = React.createRef();
-    }
+		this.pagination = React.createRef();
+		this.delete = React.createRef();
+	}
 
-    componentDidMount = () => { this.handleGetData(); }
+	componentDidMount = () => {
+		this.handleGetData();
+	}
 
-    handleGetData = () => {
-        const { perPage, sorter, filters } = this.state;
-        List.getData(this, Routing.generate(URL_GET_DATA), perPage, sorter, this.props.highlight, filters, this.handleFilters);
-    }
+	handleGetData = () => {
+		const { perPage, sorter, filters } = this.state;
+		List.getData(this, Routing.generate(URL_GET_DATA), perPage, sorter, this.props.highlight, filters, this.handleFilters);
+	}
 
-    handleUpdateData = (currentData) => { this.setState({ currentData }) }
+	handleUpdateData = (currentData) => {
+		this.setState({ currentData })
+	}
 
-    handleSearch = (search) => {
-        const { perPage, sorter, dataImmuable, filters } = this.state;
-        List.search(this, 'changelog', search, dataImmuable, perPage, sorter, true, filters, this.handleFilters)
-    }
+	handleSearch = (search) => {
+		const { perPage, sorter, dataImmuable, filters } = this.state;
+		List.search(this, 'changelog', search, dataImmuable, perPage, sorter, true, filters, this.handleFilters)
+	}
 
-    handleFilters = (filters, nData = null) => {
-        const { dataImmuable, perPage, sorter } = this.state;
-        return List.filter(this, 'type', nData ? nData : dataImmuable, filters, perPage, sorter, SESSION_FILTERS);
-    }
+	handleFilters = (filters, nData = null) => {
+		const { dataImmuable, perPage, sorter } = this.state;
+		return List.filter(this, 'type', nData ? nData : dataImmuable, filters, perPage, sorter, SESSION_FILTERS);
+	}
 
-    handleUpdateList = (element, context) => {
-        const { data, dataImmuable, currentData, sorter } = this.state;
-        List.updateListPagination(this, element, context, data, dataImmuable, currentData, sorter)
-    }
+	handleUpdateList = (element, context) => {
+		const { data, dataImmuable, currentData, sorter } = this.state;
+		List.updateListPagination(this, element, context, data, dataImmuable, currentData, sorter)
+	}
 
-    handlePaginationClick = (e) => { this.pagination.current.handleClick(e) }
+	handlePaginationClick = (e) => {
+		this.pagination.current.handleClick(e)
+	}
 
-    handleChangeCurrentPage = (currentPage) => { this.setState({ currentPage }); }
+	handleChangeCurrentPage = (currentPage) => {
+		this.setState({ currentPage });
+	}
 
-    handlePerPage = (perPage) => { List.changePerPage(this, this.state.data, perPage, this.state.sorter, SESSION_PERPAGE); }
+	handlePerPage = (perPage) => {
+		List.changePerPage(this, this.state.data, perPage, this.state.sorter, SESSION_PERPAGE);
+	}
 
-    handleModal = (identifiant, elem) => {
-        this.delete.current.handleClick();
-        this.setState({ element: elem })
-    }
+	handleModal = (identifiant, elem) => {
+		this.delete.current.handleClick();
+		this.setState({ element: elem })
+	}
 
-    render () {
-        const { highlight } = this.props;
-        const { data, currentData, element, loadingData, perPage, currentPage, filters } = this.state;
+	render () {
+		const { highlight } = this.props;
+		const { data, currentData, element, loadingData, perPage, currentPage, filters } = this.state;
 
-        let filtersItems = [
-            {value: 0, label: "Information",    id: "f-info"},
-            {value: 1, label: "Attention",      id: "f-atte"},
-            {value: 2, label: "Danger",         id: "f-dang"},
-        ]
+		let filtersItems = [
+			{ value: 0, label: "Information", id: "f-info" },
+			{ value: 1, label: "Attention", id: "f-atte" },
+			{ value: 2, label: "Danger", id: "f-dang" },
+		]
 
-        return <>
-            {loadingData
-                ? <LoaderElements />
-                : <>
-                    <div className="toolbar">
-                        <div className="col-1">
-                            <div className="filters">
-                                <Filter filters={filters} items={filtersItems} onFilters={this.handleFilters}/>
-                            </div>
-                            <Search onSearch={this.handleSearch} placeholder="Rechercher pas nom.."/>
-                        </div>
-                    </div>
+		return <>
+			{loadingData
+				? <LoaderElements />
+				: <>
+					<div className="toolbar">
+						<div className="col-1">
+							<div className="filters">
+								<Filter filters={filters} items={filtersItems} onFilters={this.handleFilters} />
+							</div>
+							<Search onSearch={this.handleSearch} placeholder="Rechercher pas nom.." />
+						</div>
+					</div>
 
-                    <TopSorterPagination taille={data.length} currentPage={currentPage} perPage={perPage}
-                                         onClick={this.handlePaginationClick}
-                                         onPerPage={this.handlePerPage} />
+					<TopSorterPagination taille={data.length} currentPage={currentPage} perPage={perPage}
+										 onClick={this.handlePaginationClick}
+										 onPerPage={this.handlePerPage} />
 
-                    <ChangelogsList data={currentData} highlight={parseInt(highlight)} onModal={this.handleModal} />
+					<ChangelogsList data={currentData} highlight={parseInt(highlight)} onModal={this.handleModal} />
 
-                    <Pagination ref={this.pagination} items={data} taille={data.length} currentPage={currentPage}
-                                perPage={perPage} onUpdate={this.handleUpdateData} onChangeCurrentPage={this.handleChangeCurrentPage}/>
+					<Pagination ref={this.pagination} items={data} taille={data.length} currentPage={currentPage}
+								perPage={perPage} onUpdate={this.handleUpdateData} onChangeCurrentPage={this.handleChangeCurrentPage} />
 
-                    <ModalDelete refModal={this.delete} element={element} routeName={URL_DELETE_ELEMENT}
-                                 title="Supprimer ce changelog" msgSuccess="Changelog supprimé"
-                                 onUpdateList={this.handleUpdateList} >
-                        Etes-vous sûr de vouloir supprimer définitivement ce changelog ?
-                    </ModalDelete>
-                </>
-            }
-        </>
-    }
+					<ModalDelete refModal={this.delete} element={element} routeName={URL_DELETE_ELEMENT}
+								 title="Supprimer ce changelog" msgSuccess="Changelog supprimé"
+								 onUpdateList={this.handleUpdateList}>
+						Etes-vous sûr de vouloir supprimer définitivement ce changelog ?
+					</ModalDelete>
+				</>
+			}
+		</>
+	}
 }
--- assets/common/js/components/Elements/Loader.jsx
+++ assets/common/js/components/Elements/Loader.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-export function LoaderElements({ text = "Chargement des données" }) {
-    return <div className="loader-content">
-        <div className="loader-simple">
-            <p>{text} <span>.</span><span>.</span><span>.</span></p>
+export function LoaderElements ({ text = "Chargement des données" }) {
+    return <div className="bg-white rounded-md shadow">
+        <div className="py-6 px-4 text-center">
+            <span className="icon-chart-3 inline-block animate-spin"></span>
+            <span className="ml-2">{text}...</span>
         </div>
     </div>
 }
