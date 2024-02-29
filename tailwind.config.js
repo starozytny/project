a5e7150d66456f9974c9896336ff13bd7e1c5756
shanbora <chanbora@logilink.fr>
2024-02-29 16:35:48 +0100

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.{js,jsx,ts,tsx}",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
+
