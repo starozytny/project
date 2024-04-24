f10dc9639f5edb9dfb9d64873e2478fcc2d79246
shanbora <chanbora@logilink.fr>
2024-04-24 16:24:38 +0200

    attribute

--- src/Controller/InternApi/Immo/ImmoController.php
+++ src/Controller/InternApi/Immo/ImmoController.php
@@ -7,7 +7,7 @@ use App\Service\ApiResponse;
 use App\Service\Immo\ImmoService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
