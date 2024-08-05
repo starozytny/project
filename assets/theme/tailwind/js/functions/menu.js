f91580fa1e4244e3492aa6034e05637f48174190
shanbora <chanbora@logilink.fr>
2024-08-05 10:42:23 +0200

    menu

--- assets/theme/tailwind/js/functions/menu.js
+++ assets/theme/tailwind/js/functions/menu.js
@@ -1,36 +1,43 @@
 function menuListener() {
-	let btn = document.querySelector('#nav-mobile');
-	if(btn){
-		let body = document.querySelector("body");
+	let body = document.querySelector("body");
+	let btns = document.querySelectorAll('.nav-mobile');
+	btns.forEach(btn => {
 		btn.addEventListener('click', function () {
-			let content = document.querySelector('#nav-content');
-			let svgs = document.querySelectorAll('#nav-mobile > svg');
-
-			svgs.forEach(svg => {
-				if(svg.classList.contains('hidden')){
-					svg.classList.remove('hidden');
-					svg.classList.add('flex');
-				}else{
-					svg.classList.remove('flex');
-					svg.classList.add('hidden');
-				}
-			})
-
-			if(content.classList.contains('hidden')){
-				content.classList.remove('hidden');
-				content.classList.add('flex');
-				content.style.overflow = "auto";
-				body.style.overflow = "hidden";
-			}else{
-				content.classList.remove('flex');
-				content.classList.add('hidden');
-				content.style.overflow = "hidden";
-				body.style.overflow = "auto";
-			}
+			menuClose(body);
 		})
+	})
+}
+
+function menuClose (body) {
+	let content = document.querySelector('#nav-content');
+	let svgs = document.querySelectorAll('#nav-mobile-main-btn > svg');
+
+	svgs.forEach(svg => {
+		if(svg.classList.contains('hidden')){
+			svg.classList.remove('hidden');
+			svg.classList.add('flex');
+		}else{
+			svg.classList.remove('flex');
+			svg.classList.add('hidden');
+		}
+	})
+
+	if(content.classList.contains('hidden')){
+		content.classList.remove('hidden');
+		content.classList.add('block');
+		content.style.zIndex = 50;
+		content.style.overflow = "auto";
+		body.style.overflow = "hidden";
+	}else{
+		content.classList.remove('block');
+		content.classList.add('hidden');
+		content.style.zIndex = -10;
+		content.style.overflow = "hidden";
+		body.style.overflow = "auto";
 	}
 }
 
 module.exports = {
-	menuListener
+	menuListener,
+	menuClose
 }
