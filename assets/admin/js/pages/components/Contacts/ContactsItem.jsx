b68d77508f53b352071690f373960d4a591dce5b
shanbora <chanbora@logilink.fr>
2025-07-15 09:26:47 +0200

    classname

--- assets/admin/js/pages/components/Contacts/ContactsItem.jsx
+++ assets/admin/js/pages/components/Contacts/ContactsItem.jsx
@@ -39,7 +39,7 @@ export function ContactsItem ({ elem, onDelete })
                     <div className="font-medium">
                         <span>{elem.name}</span>
                     </div>
-                    <div class="text-gray-600 text-xs">{elem.email}</div>
+                    <div className="text-gray-600 text-xs">{elem.email}</div>
                 </div>
                 <div className="col-2">
                     <div className="text-gray-600 text-sm">{Sanitaze.toFormatCalendar(elem.createdAt)}</div>
